64d75063f0d596c073212b684171698f2d39fd80
Unknown <eeghor@gmail.com>
2017-11-09 23:55:56 +1100

    sparse

--- chinesenames.py
+++ chinesenames.py
@@ -2,6 +2,16 @@ import pandas as pd
 from collections import Counter
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+import time
+from namefeatures import NameFeatureExtractor
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        t_start = time.time()
+        res = func(*args, **kwargs)
+        print("f: {} # elapsed time: {:.0f} m {:.0f}s".format(func.__name__.upper(), *divmod(time.time() - t_start, 60)))
+        return res
+    return wrapper
 
 class ChineseNameDetector(object):
 
@@ -21,10 +31,23 @@ class ChineseNameDetector(object):
 		assert set(self.data.columns) == set({"full_name", "is_chinese"}), print("#@#@# error! wrong column names in data csv...")
 		assert sum(list(Counter(self.data.is_chinese).values())) == len(self.data), print("#@#@# error! seems like not all names in data are labelled...")
 
+		self.nfe = NameFeatureExtractor()
 
+	@timer
+	def create_features(self):
+
+		fs = dict()
+
+		for row in self.data.sample(frac=1).iloc[:120000].iterrows():
+			#print(self.nfe.get_features(row[1].full_name)
+			fs.update({row[0]: self.nfe.get_features(row[1].full_name)})
+		
+		fs_df = pd.DataFrame.from_dict(fs, orient='index')
+		print("created a {}x{} feature data frame".format(fs_df.shape))
 
 
 if __name__ == '__main__':
 
 	cd = ChineseNameDetector()
 	print(cd.data.head())
+	cd.create_features()
---
+++ sparsehelpers.py
@@ -0,0 +1,6 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
+def dict_to_csr_matrix(d):
+	
+	ks = {k for _ in fs for k in fs[_]}
