2bf78e7ac963225346bc2a460223cebff3c38869
Igor Korostil <eeghor@gmail.com>
2017-11-11 00:12:48 +1100

    generator works

--- chinesenames.py
+++ chinesenames.py
@@ -3,8 +3,11 @@ from collections import Counter
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 import time
+import pickle
 from textfeatures import TextFeatureExtractor
 from sparsehelpers import dict_to_csr_matrix
+from sklearn.model_selection import train_test_split
+
 
 def timer(func):
     def wrapper(*args, **kwargs):
@@ -35,20 +38,45 @@ class ChineseNameDetector(object):
 		self.tfe = TextFeatureExtractor()
 		self.features = dict()
 
+	def __repr__(self):
+	 	pass
+
+	# generator that supplies chunks of data
+	def get_data_chunks(self, sdf, ch=1000):
+
+		i = 0
+
+		while i*ch <= sdf.shape[0]:
+			yield sdf[i*ch:(i+1)*ch].toarray()
+			i += 1
+
+	def train(self):
+		# split into the training and testing dats
+		X_train, X_test, y_train, y_test = train_test_split(self.data.drop('is_chinese', axis=1), 
+											self.data.is_chinese, test_size=0.33, random_state=42, stratify=self.data.is_chinese)
+
+		print(f'training set: {X_train.shape}')
+		return self
+
+	
+
+
 	@timer
 	def create_features(self):
+
+		# target as a numpy array
+		y = self.data.is_chinese.values
 
 		for row in self.data.iterrows():
 			self.features.update({row[0]: self.tfe.get_features(row[1].full_name)})
 		
 		print(f"created {len(self.features)} features")
 
-		# create a sparce matrix
-		f_names, f_smatr = dict_to_csr_matrix(self.features)
-		print(f_smatr)
-		print(f_smatr.shape)
-		# create a pandas sparse dataframe
-		sdf = pd.SparseDataFrame(f_smatr)
+		# create a sparce feature matrix
+		self.f_names, self.f_smatr = dict_to_csr_matrix(self.features)
+		
+		
+
 
 
 		return self
@@ -58,4 +86,7 @@ if __name__ == '__main__':
 
 	cd = ChineseNameDetector()
 	print(cd.data.head())
+	cd.train()
 	cd.create_features()
+	for c in cd.get_data_chunks(cd.f_smatr):
+		print(c)
--- namefeatures.py
+++ namefeatures.py
@@ -6,7 +6,7 @@ class NameFeatureExtractor(object):
 	def __init__(self):
 
 		# features will be a set of tuples (feature name, feature value)
-		self.features = set()
+		pass
 
 	def _normalise(self, st):
 		"""
@@ -23,7 +23,7 @@ class NameFeatureExtractor(object):
 		"""
 		for w, cnt in Counter(st.split()).items():
 			if len(w) > 1:
-				self.features.add((w, cnt))
+				self.features.update({w: cnt})
 
 	def _get_word_pairs(self, st):
 		"""
@@ -33,13 +33,13 @@ class NameFeatureExtractor(object):
 
 		for i, w in enumerate(ok_words):
 			if len(ok_words) > i + 1:
-				self.features.add(('-'.join([w, ok_words[i+1]]), 1))
+				self.features.update({'-'.join([w, ok_words[i+1]]): 1})
 
 	def _get_number_words(self, st):
 		"""
 		count the number of words separated by white space
 		"""
-		self.features.add(('n_words', len(st.split())))
+		self.features.update({'n_words': len(st.split())})
 
 	def _get_first_letters(self, st):
 		"""
@@ -47,21 +47,21 @@ class NameFeatureExtractor(object):
 		example: 'john j smith' shoud result in ('fst_let_j', 2), ('fst_let_s', 1)
 		"""
 		for l, cnt in Counter([w[0] for w in st.split()]).items():
-			self.features.add(('fst_let_' + l, cnt))
+			self.features.update({'fst_let_' + l: cnt})
 
 	def _get_letter_counts(self, st):
 		"""
 		count ocurrences of all letters in full name
 		"""
 		for l, cnt in Counter(st.replace(' ', '')).items():
-			self.features.add(('cnt_' + l, cnt))
+			self.features.update({'cnt_' + l: cnt})
 
 	def _get_ngram_counts(self, st, n=2):
 		"""
 		count pairs of letters (for each word in full name)
 		"""
 		for p, cnt in Counter([w[i: i+n] for w in st.split() for i in range(len(w)) if len(w[i: i+n]) == n]).items():
-			self.features.add(('cnt_' + p, cnt))
+			self.features.update({'cnt_' + p: cnt})
 
 	def _get_endings(self, st, n=2):
 		"""
@@ -69,7 +69,7 @@ class NameFeatureExtractor(object):
 		"""
 		for w in st.split():
 			if len(w) > n:
-				self.features.add(('end_' + w[-n:], 1))
+				self.features.update({'end_' + w[-n:]: 1})
 
 	def _get_word_lengths(self, st):
 		
@@ -78,7 +78,7 @@ class NameFeatureExtractor(object):
 		"""
 		ok_words = sorted([str(len(w)) for w in st.split() if len(w) > 1], reverse=True)
 		if ok_words:
-			self.features.add(('len_' + '_'.join(ok_words), 1))
+			self.features.update({'len_' + '_'.join(ok_words): 1})
 
 	def _get_frac_letters_all_words(self, st):
 		"""
@@ -89,12 +89,14 @@ class NameFeatureExtractor(object):
 		if len(ok_words) > 1:
 			inboth = set(ok_words[0]) & set(ok_words[-1])
 			if inboth:
-				self.features.add(('frac_let_both_', round(len(inboth)/len(set(''.join(ok_words))), 2)))
+				self.features.update({'frac_let_both_': round(len(inboth)/len(set(''.join(ok_words))), 2)})
 
 	def get_features(self, st):
 		"""
 		extract all features for the given full name
 		"""
+		self.features = dict()
+
 		self._get_word_counts(st)
 		self._get_number_words(st)
 		self._get_first_letters(st)
@@ -107,9 +109,9 @@ class NameFeatureExtractor(object):
 		self._get_word_lengths(st)
 		self._get_frac_letters_all_words(st)
 
-		return self
+		return self.features
 
 
 if __name__ == '__main__':
 	 ne = NameFeatureExtractor()
-	 print(ne.get_features('britney bob spears').features)
+	 print(ne.get_features('britney bob spears'))
